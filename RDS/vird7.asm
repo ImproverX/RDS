c0ae335d50a184101a42ec3f436adbb8ddadefa3
Improver <95467688+ImproverX@users.noreply.github.com>
2022-02-10 10:53:01 +0300

    Update vird7.asm

--- RDS/vird7.asm
+++ RDS/vird7.asm
@@ -1,9 +1,9 @@
 	.ORG	0080h
-	JMP	READ
-	JMP	WRITE
+A_080:	JMP	READ
+A_083:	JMP	WRITE
 READ:	SPHL
 	XCHG
-READ10:	MVI	A,10H
+READ10:	MVI	A,10H	; 0001 0000 -- режим "стек", банк 3
 	OUT	10H
 	POP	D
 	LDA	3CH
@@ -14,10 +14,11 @@ READ10:	MVI	A,10H
 	INX	H
 	DCR	C
 	JNZ	READ10
-	LDA	15
+	LDA	15	; РДС
 	OUT	10H
 	LXI	SP,SBUF	; <= 0FEH
 	RET
+;
 WRITE:	SPHL
 	XCHG
 WRIT10:	LDA	3CH
@@ -26,17 +27,17 @@ WRIT10:	LDA	3CH
 	MOV	D,M
 	DCX	H
 	MOV	E,M
-	MVI	A,10H
+	MVI	A,10H	; 0001 0000 -- режим "стек", банк 3
 	OUT	10H
 	PUSH	D
 	DCR	C
 	JNZ	WRIT10
 	LXI	SP,SBUF	; <= 0FEH
-	LDA	15
+	LDA	15	; РДС
 	OUT	10H
 	RET
-	.DS 62
-SBUF:	.DW 0
+	.ORG 00FEh
+SBUF:	.DW 0		; << стек, адрес возврата из vird7
 	.org 027Fh	; выравнивание размера
 	.db 0
 	.END
